417fabba2a7e84c6de4c63d4e5a21b84f2247125
Samuel <hackmew@gmail.com>
2018-12-04 23:10:44 +0100

    traduction FR v2

--- wbwwb/js/textStrings origin.js
+++ wbwwb/js/textStrings origin.js
@@ -310,63 +310,63 @@ var textStrings_DE = {
 
 var textStrings_FR = {
     // Act 1
-    "niceHat"              : "Ooh quel joli chapeau",
-    "outtaHere"            : "oui c'est ça allez ouste",
-    "getARoom"             : "DÉGOÛTANT, Y'A DES HÔTELS POUR ÇA",
-    "notCoolAnymore"       : "meh finalement les chapeaux c'est ringard",
-    "tvOnTv"               : "Une télé...A la télé !",
-    "cricky"               : "Petit criquet <3",
-    "tooManyCrickets"      : "ok ça commence a faire pas mal de crickets",
-    "normalPeep"           : "simplement une personne normale",
-    "normalPeeps"          : "simplement des personnes normales",
-    "wowNothing"           : "Waaaa, rien du tout!",
+    "niceHat"              : "Ooh quel joli chapeau"                                            ,
+    "outtaHere"            : "Aller, dégage de là !"                                            ,
+    "getARoom"             : "BEURK, trouvez vous une chambre"                                  ,
+    "notCoolAnymore"       : "Les chapeaux ne sont plus à la mode"                              ,
+    "tvOnTv"               : "Une télé...A la télé !"                                           ,
+    "cricky"               : "Petit criquet <3"                                                 ,
+    "tooManyCrickets"      : "ok, trop de criquets !"                                           ,
+    "normalPeep"           : "c'est juste un gars normal"                                       ,
+    "normalPeeps"          : "simplement des gars normaux"                                      ,
+    "wowNothing"           : "Waaaa, ce n'est rien..."                                          ,
     
     // Act 2
-    "crazySquareAttacks"   : "UN CARRÉ FOU COMMET UN ATTENTAT",
-    "justMissed"           : "oooooh flûte raté",
-    "somethingInteresting" : "(*baille* vraiment très intéressant.)",
-    "whoIsScreaming"       : "(où est la personne qui crie??)",
-    "circleFearsSquares"   : "LES CERCLES ONT PEUR DES CARRÉS",
-    "whoScaresThem"        : "(attendez on voit pas bien de qui il•elle a peur)",
-    "areTheyScared"        : "XXXX (il s'agit de les prendre *pendant* qu'un carré leur crie dessus)"       , // not used
-    "squaresSnubCircles"   : "LES CARRÉS SNOBENT LES CERCLES",
-    "areTheySnubbed"       : "XXXX (il s'agit de les prendre *pendant* qu'ils snobent un cercle)"              , // not used
-    "everyoneHates"        : "TOUT LE MONDE DÉTESTE TOUT LE MONDE !!!!",
-    "almostEveryoneHates"  : "PRESQUE TOUT LE MONDE DÉTESTE TOUT LE MONDE ...",
-    "squaresHateCircles"   : "LES CARRÉS DÉTESTENT LES CERCLES",
-    "circlesHateSquares"   : "LES CERCLES DÉTESTENT LES CARRÉS",
-    "areTheyYelling"       : "XXXX (il s'agit de les prendre en train de se *hurler* dessus)"                   , // not used
-    "nerdsNow"             : "que font ces nerds?",
-    "schockedPeep"         : "pourquoi est-il•elle choqué•e?",
-    "whatever"             : "peu importe",
+    "crazySquareAttacks"   : "ATTAQUE D'UN CARRÉ FOU"                                                ,
+    "justMissed"           : "oooooh, raté"                                                          ,
+    "somethingInteresting" : "(tu dois les prendre en train de faire *quelque chose* d'intéressant)" ,
+    "whoIsScreaming"       : "(tu dois prendre celui qui leurs cri dessus)"                          ,
+    "circleFearsSquares"   : "LES CERCLES SONT EFFRAYES PAR LES CARRES"                              ,
+    "whoScaresThem"        : "(tu dois aussi prendre *celui* qui les terrorise)"                     ,
+    "areTheyScared"        : "(tu dois les prendre *en train d'être* terrorisés par un carré)"       , // not used
+    "squaresSnubCircles"   : "LES CARRES SNOBENT LES RONDS"                                          ,
+    "areTheySnubbed"       : "(tu dois les prendre *pendant* qu'ils snobent un cercle)"              , // not used
+    "everyoneHates"        : "TOUT LE MONDE DÉTESTE TOUT LE MONDE !!1!"                              ,
+    "almostEveryoneHates"  : "PRESQUE TOUT LE MONDE DÉTESTE TOUT LE MONDE ..."                       ,
+    "squaresHateCircles"   : "LES CARRÉS DÉTESTENT LES CERCLES"                                      ,
+    "circlesHateSquares"   : "LES CERCLES DÉTESTENT LES CARRÉS"                                      ,
+    "areTheyYelling"       : "(tu dois les prendre en train *d'hurler* entre eux)"                   , // not used
+    "nerdsNow"             : "que font ces intellos en ce moment"                                    ,
+    "schockedPeep"         : "pourquoi ce peep est choqué?"                                          ,
+    "whatever"             : "peu importe"                                                           ,
     
     // Act 3
     "ellipsis"             : ". . ."                                                                ,
-    "coolNoMore"           : "les chapeaux ne sont plus à la mode, vous n'étiez pas au courant?"           ,
-    "beScared"             : "SOYEZ TERRIFIÉS, SOYEZ EN COLÈRE."                                      ,
+    "coolNoMore"           : "les chapeaux ne sont plus à la mode, vous ne le savez pas?"           ,
+    "beScared"             : "SOIS TERRORISÉ, SOIS EN COLÈRE."                                      ,
     
     // MANIFESTO
     "manifesto" : [
        //"as if you viewers want GOOD news"       ,
-       "qui suivrait les infos pour regarder des *gens qui s'entendent bien?*"   ,
-       "la paix c'est pas très excitant. Ce que les gens veulent, c'est du sang."      ,
+       "qui se connecte pour regarder des *gens s'entendre?*"   ,
+       "la paix est d'un ennui, la violence fait le buzz."      ,
        //"peace is boring. conflict gets clicks." ,
-       "et chaque histoire a besoin de tension, de conflit, alors..."     ,
+       "et chaque histoire a besoin d'un conflit, alors..."     ,
        //"...GIVE THE AUDIENCE WHAT THEY WANT."   ,
-       "DONNEZ AU PUBLIC CE QU'IL RÉCLAME."
+       "DONNE AU PUBLIC CE QU'IL VEUT."
     ] ,
     
     // -- Strings currently in graphics resources --
     
     // (bg_preload.png) Preload screen
     "playingTime"        : "temps de jeu: 5 minutes" ,
-    "warning"            : "attention: ce jeu\ncontient des scènes d'arrogance,\nde gens qui crient,\net de meurtres en série.\n il s'adresse donc \nà un public majeur averti." ,
+    "warning"            : "attention: le programme contient des scènes de dénigrement, de violence verbale et physique et notamment, de meurtres de masse.\n ce programme s'adresse à un public majeur averti." ,
     
     // (cam-instructions.png) Camera Instructions
-    "pointAndClick"      : "POINT & CLICK" ,
+    "pointAndClick"      : "POINTEZ & CLIQUEZ" ,
     
     // (chyron3.png)
-    "chyronNothing"      : "WAAAA, *RIEN DU TOUT*" ,
+    "chyronNothing"      : "WAAAA, CE N'EST *RIEN*" ,
     
     // (credits0001.png)
     "createdBy"          : "créé par" ,
@@ -376,7 +376,7 @@ var textStrings_FR = {
     "manyThanks"         : "mille mercis à mes testeurs:" ,
     
     // (credits000[3,4,5,6].png)
-    "patreonSupporters"  : "et à mes soutiens sur Patreon:" ,
+    "patreonSupporters"  : "et à mes soutiens sur Patreons:" ,
     
     // (credits0007.png)
     "lastButNotLeast"    : "et pour finir," ,
@@ -389,8 +389,8 @@ var textStrings_FR = {
     
     // (end_button.png) Post Credits
     "otherWorkButton"    : "mes autres réalisations" ,
-    "buyCoffeeButton"    : "payez moi un café" ,
-    "replayButton"       : "rejouer à ce truc" ,
+    "buyCoffeeButton"    : "paye moi un café" ,
+    "replayButton"       : "rejoue ce truc" ,
     
     // (logo.png) Post Credits
     "logoWBWWB"          : "PARTAGE LA DOULEUR:" ,
@@ -400,14 +400,14 @@ var textStrings_FR = {
     "toBeContinued"      : "(à suivre!)" ,
     
     // (quote0002.png)
-    "WBWWB"              : "We become what we behold." ,
-    "WSOTATOTSU"         : "Nous fabriquons des outils, et ensuite ces outils nous fabriquent à leur tour." ,
+    "WBWWB"              : "Nous devenons ce que nous regardons." ,
+    "WSOTATOTSU"         : "Les outils que nous créons, finissent par nous façonner." ,
     
     // (quote0003.png)
     "MarshallMcLuhan"    : "Marshall McLuhan" ,
     
     // (quote0004.png)
-    "misatrributed"      : "(en tout cas ça pourrait être de lui)" ,
+    "misatrributed"      : "(attribué par erreur)" ,
     
 };
 
